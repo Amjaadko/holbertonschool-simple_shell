e2d715bf6ef2c4c2e62279d3a0f8762480879328
Amjad Al Omani <iamjaadk@gmail.com>
2025-11-09 16:26:25 +0000

    Complete simple shell up to 1.0 with builtins

--- try_builtin.c
+++ try_builtin.c
@@ -1,18 +1,43 @@
 #include "simple_01.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
- * try_builtin - Placeholder for built-in commands
- * @argv: command arguments
- * @last_status: pointer to last status
- * @should_exit: pointer to exit flag
- *
- * Return: 0 (no built-in executed)
+ * try_builtin - handle built-in shell commands
+ * @argv: array of command arguments
+ * @last_status: pointer to store last command status
+ * @should_exit: pointer to flag indicating exit
+ * Return: 1 if a built-in command was executed, 0 otherwise
  */
 int try_builtin(char **argv, int *last_status, int *should_exit)
 {
-    (void)argv;
-    (void)last_status;
-    (void)should_exit;
-    return (0); /* no built-in executed */
+	*should_exit = 0;
+
+	if (argv[0] == NULL)
+		return (0);
+
+	if (strcmp(argv[0], "exit") == 0)
+	{
+		*should_exit = 1;
+		*last_status = 0;
+		return (1);
+	}
+
+	if (strcmp(argv[0], "env") == 0)
+	{
+		extern char **environ;
+		int i = 0;
+
+		while (environ[i])
+		{
+			printf("%s\n", environ[i]);
+			i++;
+		}
+		*last_status = 0;
+		return (1);
+	}
+
+	return (0);
 }
 
